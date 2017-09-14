4f4a6a58913a3ee5852dc803d93272c64cb4f30d
kba977 <kba977@gmail.com>
2017-09-14 17:00:02 +0800

    add [python]hamming

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,2 +1,8 @@
-def distance():
-    pass
+def distance(seq1, seq2):
+    L = len(seq1)
+    if L != len(seq2):
+        raise ValueError
+    if L == 0:
+        return 0
+    dist = sum(c1 != c2 for c1, c2 in zip(seq1, seq2))
+    return dist
